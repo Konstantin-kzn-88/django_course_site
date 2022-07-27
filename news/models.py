d523af70915015a8ef7ff2e3e5ec658f0f4ae1fd
konstantin <kuznetsovkm@yandex.ru>
2022-07-27 22:08:00 +0300

    Модели

--- news/models.py
+++ news/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True) # рег.даты создания
+    updated_at = models.DateTimeField(auto_now=True) # рег.даты изменения
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/') # параметр показывает куда сохранять
+    is_published = models.BooleanField(default=True)
