f216afda5f6a4d6d6d514010ee6b0c9208614154
konstantin <kuznetsovkm@yandex.ru>
2022-07-28 22:37:38 +0300

    CRUD

--- news/models.py
+++ news/models.py
@@ -8,3 +8,6 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True) # рег.даты изменения
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/') # параметр показывает куда сохранять
     is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
