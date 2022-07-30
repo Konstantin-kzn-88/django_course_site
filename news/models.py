f6d3b88895a3968e134cf2389ad2aadc58b2122a
konstantin <kuznetsovkm@yandex.ru>
2022-07-30 10:11:09 +0300

    Связи моделей

--- news/models.py
+++ news/models.py
@@ -4,10 +4,12 @@ from django.db import models
 class News(models.Model):
     title = models.CharField(max_length=150, verbose_name='Заголовок')
     content = models.TextField(blank=True, verbose_name='Контент')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации') # рег.даты создания
-    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата изменения') # рег.даты изменения
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True) # параметр показывает куда сохранять
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')  # рег.даты создания
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')  # рег.даты изменения
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото',
+                              blank=True)  # параметр показывает куда сохранять
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано?')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
 
     def __str__(self):
         return self.title
@@ -16,3 +18,15 @@ class News(models.Model):
         verbose_name = 'Новость'
         verbose_name_plural = 'Новости'
         ordering = ['-created_at', '-title']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование категории')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
