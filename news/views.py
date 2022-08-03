811a55b4dc5e5fb37df07ae67730c397faea2829
Konstantin <kuznetsovkm@yandex.ru>
2022-08-03 10:52:20 +0300

    Пользовательские теги шаблона

--- news/views.py
+++ news/views.py
@@ -5,21 +5,17 @@ from .models import News, Category
 
 def index(request):
     news = News.objects.order_by('-created_at')
-    categories = Category.objects.all()
     context = {'news': news,
-               'title': 'Список новостей',
-               'categories': categories}
+               'title': 'Список новостей'}
     return render(request,
                   template_name='news/index.html',
                   context=context)
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
     context = {'news': news,
-               'category': category,
-               'categories': categories}
+               'category': category}
     return render(request,
                   template_name='news/category.html',
                   context=context)
