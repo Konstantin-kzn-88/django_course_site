747f8c4195fb0b7e05f5ac2808a74a4f7dad1082
konstantin <kuznetsovkm@yandex.ru>
2022-08-03 21:19:24 +0300

    Обратное разрешение адресов

--- news/views.py
+++ news/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 from .models import News, Category
 
@@ -11,6 +12,7 @@ def index(request):
                   template_name='news/index.html',
                   context=context)
 
+
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
     category = Category.objects.get(pk=category_id)
@@ -20,3 +22,8 @@ def get_category(request, category_id):
                   template_name='news/category.html',
                   context=context)
 
+
+def view_news(request, news_id):
+    # item = News.objects.get(pk=news_id)
+    item = get_object_or_404(News, pk = news_id)
+    return render(request, 'news/view_news.html', {'item': item})
