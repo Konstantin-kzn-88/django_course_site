ae5c5f284606d392d4d26b050e310374580ddd9c
konstantin <kuznetsovkm@yandex.ru>
2022-08-06 10:13:07 +0300

    Формы Часть 2 форма не связанная с моделью. Сохранение + бутстрап

--- news/views.py
+++ news/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 from .forms import NewsForm
 from .models import News, Category
 
@@ -30,7 +30,10 @@ def view_news(request, news_id):
 
 def add_news(request):
     if request.method == 'POST':
-        ...
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            news = News.objects.create(**form.cleaned_data)
+            return redirect(news)
     else:
         form = NewsForm()
     return render(request, 'news/add_news.html', {'form': form})
